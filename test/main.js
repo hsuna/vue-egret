a70ca3a1b8d6c97bfb95029956644af2dd42ce7c
hsuna <me@hsuna.com>
2021-03-17 11:14:08 +0800

    test: update test ref

--- test/main.js
+++ test/main.js
@@ -2,14 +2,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import VueEgret from 'vue-egret';
 
-const examples = [
-  require('./examples/test1.js').default,
-  require('./examples/test2.js').default,
-  require('./examples/watch.js').default,
-  require('./examples/set.js').default,
-  require('./examples/event.js').default,
-  require('./examples/tick.js').default,
-  require('./examples/directive.js').default,
-];
-
-window.Main = VueEgret.classMain(examples[Number(window.location.hash.slice(1) || '')]);
+window.Main = VueEgret.classMain(
+  require(`./examples/${window.location.hash.slice(1) || 'test'}.js`).default,
+);
